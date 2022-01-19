2b5ba058626a0b62862e87489a5687782f63c6c6
eagle <osu.eagle@gmail.com>
2022-01-19 15:52:18 +0200

    init project

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+/***************
+ * DB CONNECT CONFIG
+ */
+
+const DB_HOST = 'locslhost';
+const DB_USER = '';
+const DB_PASS = '';
+const DB_NAME = '';
---
+++ vendor/Router.php
@@ -0,0 +1,7 @@
+<?php
+
+
+class Router
+{
+ static public function init{}
+}
